9ea7e26ee66fff1391efa7686cecbb98b849bc96
Alex Meci <mecialex@gmail.com>
2022-10-13 13:19:16 -0400

    Updates

---
+++ precache-manifest.63d8aeee1887ef240b04a45dff0148d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11ad1e4a0a762a7ad83a3767058e8f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec825434d04064cb8866",
+    "url": "/static/css/main.5d10e19b.chunk.css"
+  },
+  {
+    "revision": "61f53813b76f8d2e6c4d",
+    "url": "/static/js/2.f4aefa36.chunk.js"
+  },
+  {
+    "revision": "2d0b1d8684219d74fa85e83ebee3f72a",
+    "url": "/static/js/2.f4aefa36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec825434d04064cb8866",
+    "url": "/static/js/main.a2f7945a.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
